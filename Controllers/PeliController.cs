f885201a8894f30f246c33af936a3f937d004bd1
Fikri Adriansyah <grizzsad6969@gmail.com>
2021-01-24 23:52:31 +0700

    A

---
+++ Controllers/PeliController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Belajar_Github.Controllers
+{
+    public class PeliController : Controller
+    {
+        // GET: Peli
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
